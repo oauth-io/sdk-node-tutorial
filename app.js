d8cc04887f2f96aea6f43b8aa4203dece3805f96
Antoine Jackson <a.j.william26@gmail.com>
2014-05-09 18:26:43 +0200

    added code for step 1 in app.js

--- app.js
+++ app.js
@@ -2,27 +2,34 @@ var express = require('express');
 
 /* Requiring the lib */
 
-// Require oauthio here (done in step-2)
+var oauth = require('oauthio');
 
 var app = express();
 
 app.use(express.static('public'));
 
 /* Initialization */
+try {
+	var config = require('./config');	
+} catch (e) {
+	// Create a config.js file returning an object like the following if you haven't done it yet
+	var config = {
+		key: 'your_key',
+		secret: 'your_secret'
+	};
+}
 
-
-// Require oauthio here (done in step-2)
-
+oauth.initialize(config.key, config.secret);
 
 /* Endpoints */
 
-// Add a .get endpoint for the state token here (/oauth/token) (done in step-3)
+// Add a .get endpoint for the state token here (/oauth/token) (done in step-2)
 
 
-// Add a .post endpoint for the state token here (/oauth/signin) (done in step-4)
+// Add a .post endpoint for the state token here (/oauth/signin) (done in step-3)
 
 
-// Add a .get endpoint for the request here (/me) (done in step-5)
+// Add a .get endpoint for the request here (/me) (done in step-4)
 
 app.listen(process.env.NODEJS_PORT || 3000, function () {
 	console.log('OAuth.io Tutorial server running on port ' + (process.env.NODEJS_PORT || 3000));
