2e45f7488632e8c20a610dc00e0a4b22bc5405e5
Antoine Jackson <a.j.william26@gmail.com>
2014-05-09 18:00:06 +0200

    initial commit on step-0

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+
+/* Requiring the lib */
+
+// Require oauthio here (done in step-1)
+
+var app = express();
+
+app.use(express.static('public'));
+
+/* Initialization */
+
+
+// Initialize oauthio here
+
+
+/* Endpoints */
+
+// Add a .get endpoint for the state token here (/oauth/token) (done in step-2)
+
+
+// Add a .post endpoint for the state token here (/oauth/signin) (done in step-3)
+
+
+// Add a .get endpoint for the request here (/me) (done in step-4)
+
+app.listen(process.env.NODEJS_PORT || 3000, function () {
+	console.log('OAuth.io Tutorial server running on port ' + (process.env.NODEJS_PORT || 3000));
+});
---
+++ public/src/script.js
@@ -0,0 +1,13 @@
+// Here you need to put the front-end code
+
+function init_oauthio() {
+
+}
+
+function register_login_button() {
+
+}
+
+function retrieve_user_info() {
+	
+}
